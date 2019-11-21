70f71eb7e406372e1c5f8a74166f6fe62777e64d
Laura Szűcs <laura.szucs2@stud.uni-corvinus.hu>
2019-11-21 11:06:41 +0100

    Third commit

--- UserMaintanance/UserMaintanance/Form1.cs
+++ UserMaintanance/UserMaintanance/Form1.cs
@@ -17,8 +17,8 @@ namespace UserMaintanance
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName;
-            label2.Text = Resource1.FirstName;
+            label1.Text = Resource1.FullName;
+            //label2.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
 
             listBox1.DataSource = users;
@@ -27,8 +27,7 @@ namespace UserMaintanance
 
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text
             };
             users.Add(u);
         }
